34cf79293dd1471fa06110e7df8b8eb00cd89f69
Nemanja Subotic <subinesa@gmail.com>
2016-12-21 13:11:35 +0100

    Include bootstrap-material-design

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import angular from 'angular';
+
+global.$ = global.jQuery = require('jquery');
+global.moment = require('moment');
+
+// CSS files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-material-design/dist/css/bootstrap-material-design.css";
+import "bootstrap-material-design/dist/css/ripples.css";
+import "bootstrap-material-design/dist/js/material.js";
+import 'bootstrap-material-design/dist/js/ripples.js';
+
+// Application modules
 import uirouter from 'angular-ui-router';
-
 import routing from './app.config';
-
 import home from './features/home';
 
-
 angular.module('app', [uirouter, home])
-  .config(routing);
+    .config(routing);
+
+$(function() {
+    $.material.init();
+    $.material.ripples();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
     context: __dirname + '/src',
     entry: './app.js',
@@ -6,11 +8,33 @@ module.exports = {
         filename: './bundle.js'
     },
     module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.html$/, loader: "html" },
-            { test: /\.css$/, loader: 'style!css' },
-            { test: /\.scss$/, loader: 'style!css!sass' }
-        ]
-    }
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }, {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }, {
+            test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
+            loader: 'file-loader?name=res/[name].[ext]?[hash]'
+        }, {
+            test: /\.html/,
+            loader: 'raw'
+        }, {
+            test: /\.json/,
+            loader: 'json'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery'
+        })
+    ]
 };
